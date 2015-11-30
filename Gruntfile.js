7f8eb9ca1481c6f165ea6b3d5bf0554f3e2a0f86
tfrommen <info@tfrommen.de>
2015-11-30 21:44:11 +0100

    Update static analysis tools.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,173 @@
+/* jshint node:true */
 module.exports = function( grunt ) {
+	'use strict';
+
+	var _ = require( 'lodash' );
 
 	var configObject = {
 		config: {
-			file         : 'default-post-date.php',
-			glotpress_url: 'http://translate.tfrommen.de',
-			languages    : 'languages/',
-			name         : 'Default Post Date',
-			repository   : 'https://github.com/tfrommen/default-post-date',
-			slug         : 'default-post-date',
-			src_path     : 'src/',
-			textdomain   : 'default-post-date'
+			assets: {
+				src: 'resources/assets/',
+				dest: 'assets/'
+			},
+			dest: 'src/',
+			glotpress: {
+				url: 'http://translate.tfrommen.de',
+				slug: 'default-post-date'
+			},
+			languages: {
+				dest: 'src/languages/',
+				dir: 'languages/'
+			},
+			plugin: {
+				file: 'default-post-date.php',
+				issues: 'https://github.com/tfrommen/default-post-date/issues',
+				name: 'Default Post Date',
+				textdomain: 'default-post-date'
+			},
+			scripts: {
+				src: 'resources/js/',
+				dest: 'src/assets/js/'
+			},
+			tests: {
+				phpunit: 'tests/phpunit/'
+			}
 		},
 
-		// https://github.com/markoheijnen/grunt-glotpress
+		// Will soon be used for QUnit.
+		clean: {},
+
+		concat: {
+			options: {
+				separator: '\n'
+			},
+			admin: {
+				src: [
+					'<%= config.scripts.src %>admin.js',
+					'<%= config.scripts.src %>admin/*.js'
+				],
+				dest: '<%= config.scripts.dest %>admin.js'
+			}
+		},
+
 		glotpress_download: {
 			languages: {
 				options: {
-					domainPath: '<%= config.src_path %><%= config.languages %>',
-					url       : '<%= config.glotpress_url %>',
-					slug      : '<%= config.slug %>',
-					textdomain: '<%= config.textdomain %>'
+					url: '<%= config.glotpress.url %>',
+					slug: '<%= config.glotpress.slug %>',
+					domainPath: '<%= config.languages.dest %>',
+					textdomain: '<%= config.plugin.textdomain %>'
 				}
 			}
 		},
 
-		// https://github.com/gruntjs/grunt-contrib-jshint
-		jshint: {
-			grunt: {
-				src: [ 'Gruntfile.js' ]
+		// TODO: Somehow make "newer:imagemin:*" work...?! Currently, always all images get minified.
+		imagemin: {
+			options: {
+				optimizationLevel: 7
+			},
+			assets: {
+				expand: true,
+				cwd: '<%= config.assets.src %>',
+				src: [ '*.{gif,jpeg,jpg,png}' ],
+				dest: '<%= config.assets.dest %>'
 			}
 		},
 
-		// https://github.com/cedaro/grunt-wp-i18n
+		jscs: {
+			options: {
+				config: true
+			},
+			grunt: {
+				src: [ 'Gruntfile.js' ]
+			},
+			scripts: {
+				src: [ '<%= config.scripts.src %>**/*.js' ]
+			}
+		},
+
+		jshint: {
+			options: {
+				jshintrc: true,
+				reporter: require( 'jshint-stylish' )
+			},
+			grunt: {
+				src: [ 'Gruntfile.js' ]
+			},
+			scripts: {
+				src: [ '<%= config.scripts.src %>**/*.js' ]
+			}
+		},
+
+		jsonlint: {
+			configs: {
+				src: [ '.{jscs,jshint}rc' ]
+			},
+			json: {
+				src: [ '*.json' ]
+			}
+		},
+
+		jsvalidate: {
+			options: {
+				globals: {},
+				esprimaOptions: {},
+				verbose: false
+			},
+			src: {
+				src: [ '<%= config.scripts.src %>**/*.js' ]
+			},
+			dest: {
+				src: [ '<%= config.scripts.dest %>**/*.js' ]
+			}
+		},
+
+		lineending: {
+			options: {
+				eol: 'lf',
+				overwrite: true
+			},
+			grunt: {
+				src: [ 'Gruntfile.js' ]
+			},
+			scripts: {
+				src: [ '<%= config.scripts.dest %>*.js' ],
+				dest: '<%= config.scripts.dest %>'
+			}
+		},
+
 		makepot: {
 			pot: {
 				options: {
-					cwd        : '<%= config.src_path %>',
-					domainPath : '<%= config.languages %>',
-					mainFile   : '<%= config.file %>',
-					potComments: 'Copyright (C) {{year}} <%= config.name %>\nThis file is distributed under the same license as the <%= config.name %> package.',
-					potFilename: '<%= config.textdomain %>.pot',
-					potHeaders : {
-						poedit                 : true,
-						'report-msgid-bugs-to' : '<%= config.repository %>/issues',
+					cwd: '<%= config.dest %>',
+					domainPath: '<%= config.languages.dir %>',
+					mainFile: '<%= config.plugin.file %>',
+					potComments: 'Copyright (C) {{year}} <%= config.plugin.name %>\nThis file is distributed under the same license as the <%= config.plugin.name %> package.',
+					potFilename: '<%= config.plugin.textdomain %>.pot',
+					potHeaders: {
+						poedit: true,
+						'report-msgid-bugs-to': '<%= config.plugin.issues %>',
 						'x-poedit-keywordslist': true
 					},
-					processPot : function( pot ) {
+					processPot: function( pot ) {
 						var exclude = [
 							'Plugin Name of the plugin/theme',
 							'Plugin URI of the plugin/theme',
 							'Author of the plugin/theme',
-							'Author URI of the plugin/theme',
-						    'translators: do not translate'
+							'Author URI of the plugin/theme'
 						];
 
-						// Skip translations with the above defined meta comments
-						for ( var translation in pot.translations[ '' ] ) {
+						var translation;
+						for ( translation in pot.translations[ '' ] ) {
+							if ( ! pot.translations[ '' ].hasOwnProperty( translation ) ) {
+								continue;
+							}
+
 							if ( 'undefined' === typeof pot.translations[ '' ][ translation ].comments.extracted ) {
 								continue;
 							}
 
+							// Skip translations with the above defined meta comments.
 							if ( exclude.indexOf( pot.translations[ '' ][ translation ].comments.extracted ) >= 0 ) {
 								delete pot.translations[ '' ][ translation ];
 							}
@@ -71,28 +179,174 @@ module.exports = function( grunt ) {
 			}
 		},
 
-		// https://github.com/gruntjs/grunt-contrib-watch
+		phplint: {
+			options: {
+				phpArgs: {
+					'-lf': null
+				}
+			},
+			file: {
+				src: [ '<%= config.plugin.file %>' ]
+			},
+			src: {
+				src: [ '<%= config.dest %>**/*.php' ]
+			},
+			tests: {
+				src: [ '<%= config.tests.phpunit %>**/*.php' ]
+			}
+		},
+
+		uglify: {
+			options: {
+				ASCIIOnly: true
+			},
+			scripts: {
+				expand: true,
+				cwd: '<%= config.scripts.dest %>',
+				src: [ '*.js', '!*.min.js' ],
+				dest: '<%= config.scripts.dest %>',
+				ext: '.min.js'
+			}
+		},
+
 		watch: {
 			options: {
-				dot     : true,
-				spawn   : true,
+				dot: true,
+				spawn: true,
 				interval: 2000
 			},
-			grunt  : {
-				files: 'Gruntfile.js',
-				tasks: [ 'jshint:grunt' ]
+
+			assets: {
+				files: [ '<%= config.assets.src %>*.{gif,jpeg,jpg,png}' ],
+				tasks: [
+					'newer:imagemin:assets'
+				]
+			},
+
+			configs: {
+				files: [ '.{jscs,jshint}rc' ],
+				tasks: [
+					'jsonlint:configs'
+				]
+			},
+
+			grunt: {
+				files: [ 'Gruntfile.js' ],
+				tasks: [
+					'jscs:grunt',
+					'jshint:grunt',
+					'lineending:grunt'
+				]
+			},
+
+			json: {
+				files: [ '*.json' ],
+				tasks: [
+					'jsonlint:json'
+				]
+			},
+
+			php: {
+				files: [
+					'<%= config.plugin.file %>',
+					'<%= config.dest %>**/*.php',
+					'<%= config.tests.phpunit %>**/*.php'
+				],
+				tasks: [
+					'phplint'
+				]
+			},
+
+			scripts: {
+				files: [ '<%= config.scripts.src %>**/*.js' ],
+				tasks: [
+					'jsvalidate:src',
+					'jshint:force',
+					'jscs:force',
+					'newer:concat',
+					'newer:lineending:scripts',
+					'newer:uglify',
+					'jsvalidate:dest'
+				]
 			}
 		}
 	};
 
-	grunt.initConfig( configObject );
+	// Add development target for JSCS.
+	configObject.jscs.force = _.merge(
+			{},
+			configObject.jscs.scripts,
+			{
+				options: {
+					force: true
+				}
+			}
+	);
 
-	// https://github.com/sindresorhus/load-grunt-tasks
+	// Add development target for JSHint.
+	configObject.jshint.force = _.merge(
+			{},
+			configObject.jshint.scripts,
+			{
+				options: {
+					devel: true,
+					force: true
+				}
+			}
+	);
+
 	require( 'load-grunt-tasks' )( grunt );
 
-	grunt.registerTask( 'default', [ 'watch' ] );
-	grunt.registerTask( 'grunt', [ 'jshint:grunt' ] );
-	grunt.registerTask( 'languages', [ 'makepot', 'glotpress_download' ] );
-	grunt.registerTask( 'production', [ 'languages' ] );
-	grunt.registerTask( 'test', [ 'jshint' ] );
+	grunt.initConfig( configObject );
+
+	grunt.registerTask( 'assets', configObject.watch.assets.tasks );
+
+	grunt.registerTask( 'configs', configObject.watch.configs.tasks );
+
+	grunt.registerTask( 'grunt', configObject.watch.grunt.tasks );
+
+	grunt.registerTask( 'json', configObject.watch.json.tasks );
+
+	grunt.registerTask( 'languages', [
+		'makepot',
+		'glotpress_download'
+	] );
+
+	grunt.registerTask( 'php', configObject.watch.php.tasks );
+
+	grunt.registerTask( 'scripts', [
+		'jsvalidate:src',
+		'jshint:scripts',
+		'jscs:scripts',
+		'newer:concat',
+		'newer:lineending:scripts',
+		'newer:uglify',
+		'jsvalidate:dest'
+	] );
+
+	grunt.registerTask( 'forcescripts', configObject.watch.scripts.tasks );
+
+	grunt.registerTask( 'lint', [
+		'jshint',
+		'jsonlint',
+		'phplint'
+	] );
+
+	grunt.registerTask( 'precommit', [
+		'assets',
+		'configs',
+		'grunt',
+		'json',
+		'languages',
+		'php',
+		'scripts'
+	] );
+
+	grunt.registerTask( 'default', [
+		'configs',
+		'grunt',
+		'json',
+		'php',
+		'forcescripts'
+	] );
 };
